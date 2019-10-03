d205ab569a327692ad459c4c520d6c83abc5bc18
Kuanysh Boranbayev <boranbayev.k@gmail.com>
2019-10-03 13:42:32 -0700

    added SearchActivityTest, but it should fail as stated in assignment requirements

--- app/src/androidTest/java/com/example/photogalleryapp/MainActivityTest.java
+++ app/src/androidTest/java/com/example/photogalleryapp/MainActivityTest.java
@@ -18,6 +18,7 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.Intents.intending;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -58,5 +59,13 @@ public class MainActivityTest {
         intended(toPackage("com.android.camera2"));
 
         Intents.assertNoUnverifiedIntents();
+    }
+
+    @Test
+    public void testSearchActivityLaunch() {
+        // click on show search button
+        onView(withId(R.id.btnFilter)).check(matches(withText("filter")));
+        // check searchActivity launch
+        intended(hasComponent(SearchActivity.class.getName()));
     }
 }

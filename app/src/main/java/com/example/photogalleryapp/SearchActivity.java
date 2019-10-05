4494ca6b09d1424f85a79524affb4bed78c8dae6
Kuanysh Boranbayev <boranbayev.k@gmail.com>
2019-10-04 22:11:33 -0700

    Search by Date Feature updates

--- app/src/main/java/com/example/photogalleryapp/SearchActivity.java
+++ app/src/main/java/com/example/photogalleryapp/SearchActivity.java
@@ -1,6 +1,5 @@
 package com.example.photogalleryapp;
 
-import android.app.DatePickerDialog;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Environment;
@@ -8,8 +7,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,7 +18,6 @@ import java.io.FilenameFilter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 
 public class SearchActivity extends AppCompatActivity {
@@ -26,22 +25,18 @@ public class SearchActivity extends AppCompatActivity {
     private Button btnSearch;
     private EditText fromDate;
     private EditText toDate;
-    private Calendar fromCalendar;
-    private Calendar toCalendar;
-    private DatePickerDialog.OnDateSetListener fromListener;
-    private DatePickerDialog.OnDateSetListener toListener;
     private Date fDate;
     private Date tDate;
-    private GridLayout searchResultDisplay;
+    private LinearLayout searchResultDisplay;
 
-    private ArrayList<String> searchResult;
+    private ArrayList<File> searchResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        fromDate = (EditText) findViewById(R.id.search_fromDate);
-        toDate   = (EditText) findViewById(R.id.search_toDate);
+        fromDate = findViewById(R.id.search_fromDate);
+        toDate   = findViewById(R.id.search_toDate);
 
         searchResultDisplay = findViewById(R.id.search_result);
 
@@ -54,18 +49,11 @@ public class SearchActivity extends AppCompatActivity {
         });
     }
 
-
-    public void cancel(final View v) {
-
-    }
-
-    public void search(final View v) {
-        displayPhoto(getPictures(fromDate.getText().toString(), toDate.getText().toString()));
-    }
-
-    public ArrayList<String> getPictures(String fromDate, String toDate) {
+    public ArrayList<File> getPictures(String fromDate, String toDate) {
         File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File[] allImages = {};
+        File[] allImages = null;
+
+        searchResult = new ArrayList<>(); // stores all images results by date search and returns
         try {
             fDate = new SimpleDateFormat("yy/MM/dd").parse(fromDate);
             tDate = new SimpleDateFormat("yy/MM/dd").parse(toDate);
@@ -73,7 +61,6 @@ public class SearchActivity extends AppCompatActivity {
             Log.v("Exception", ex.getLocalizedMessage());
         }
 
-        searchResult = new ArrayList<>();
         if (dir.exists()) {
             allImages = dir.listFiles(new FilenameFilter() {
                public boolean accept(File dir, String name) {
@@ -91,7 +78,7 @@ public class SearchActivity extends AppCompatActivity {
                 Log.d("imageDate = ", imageDate.toString());
                 if (imageDate.compareTo(fDate) > 0 && imageDate.compareTo(tDate) < 0) {
                     Log.d("Selected image: ", image.getName());
-                    searchResult.add(image.getAbsolutePath());
+                    searchResult.add(image);
                 }
             } catch (ParseException ex) {
                 Log.v("Exception", ex.getLocalizedMessage());
@@ -100,12 +87,22 @@ public class SearchActivity extends AppCompatActivity {
         return searchResult;
     }
 
-    private void displayPhoto(ArrayList<String> searchResults) {
-        for (String result: searchResults) {
+    private void displayPhoto(ArrayList<File> searchResults) {
+        for (File result: searchResults) {
+            LinearLayout ll = new LinearLayout(SearchActivity.this);
             ImageView iv = new ImageView(SearchActivity.this);
-            Log.d("displayPhoto====>", result);
-            iv.setImageBitmap(BitmapFactory.decodeFile(result));
-            searchResultDisplay.addView(iv);
+            TextView tv = new TextView(SearchActivity.this);
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(400, 400);
+            Log.d("displayPhoto====>", result.getName());
+            // ImageView for image itself
+            iv.setImageBitmap(BitmapFactory.decodeFile(result.getAbsolutePath()));
+            iv.setLayoutParams(lp);
+            // TextView for image name
+            tv.setText(result.getName());
+            ll.setOrientation(LinearLayout.HORIZONTAL);
+            ll.addView(iv);
+            ll.addView(tv);
+            searchResultDisplay.addView(ll);
         }
     }
 }
